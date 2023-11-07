5ee95c88ed6150c0f89046d50f8be59056f4d95a
AD-laD <81196308+AD-laD@users.noreply.github.com>
2023-11-07 12:17:35 +0100

    Add files via upload

---
+++ build/CMakeFiles/conic.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/adfer/Documents/IMAC_WINDOWS/IMAC2/coniques/src/Directory.cpp" "CMakeFiles/conic.dir/src/Directory.cpp.obj" "gcc" "CMakeFiles/conic.dir/src/Directory.cpp.obj.d"
+  "C:/Users/adfer/Documents/IMAC_WINDOWS/IMAC2/coniques/src/Entry.cpp" "CMakeFiles/conic.dir/src/Entry.cpp.obj" "gcc" "CMakeFiles/conic.dir/src/Entry.cpp.obj.d"
+  "C:/Users/adfer/Documents/IMAC_WINDOWS/IMAC2/coniques/src/Geogebra_conics.cpp" "CMakeFiles/conic.dir/src/Geogebra_conics.cpp.obj" "gcc" "CMakeFiles/conic.dir/src/Geogebra_conics.cpp.obj.d"
+  "C:/Users/adfer/Documents/IMAC_WINDOWS/IMAC2/coniques/src/main.cpp" "CMakeFiles/conic.dir/src/main.cpp.obj" "gcc" "CMakeFiles/conic.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
